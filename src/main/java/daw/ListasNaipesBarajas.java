52b6bce18c72406b9549e7195bb541c5fcc49cb9
acebedo <jaceara2006@g.educaand.es>
2023-12-21 16:32:06 +0100

    Inicio del Proyecto

---
+++ src/main/java/daw/ListasNaipesBarajas.java
@@ -0,0 +1,12 @@
+package daw;
+
+/**
+ *
+ * @author acebedo
+ */
+public class ListasNaipesBarajas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
